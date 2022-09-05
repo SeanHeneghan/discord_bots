92380603b005565c61d5b7e64fba760ec49c473e
SeanHeneghan <sean.heneghan@outlook.com>
2022-09-05 16:44:54 +0100

    changed main file to create driver instance differently. Added a way to seed the data in. Just need to reference the seed data now.

--- league_bot.py
+++ league_bot.py
@@ -4,125 +4,130 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from io import BytesIO
-
 
 intents = discord.Intents.all()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f"We have logged in as {client.user}")
+
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-    
+
     if message.content.startswith("__"):
-        cmd = message.content.split()[0].replace('__', '')
-        
-        if cmd == 'runes':
+        cmd = message.content.split()[0].replace("__", "")
+
+        if cmd == "runes":
             if len(message.content.split()) > 1:
                 champion = message.content.split()[1]
                 if type(champion) != str or len(message.content.split()) > 2:
                     await message.channel.send("Please only call the function via __*action* *champion* *game_mode*.")
                 else:
                     try:
-                        url = f'https://u.gg/lol/champions/{champion}/build'
+                        url = f"https://u.gg/lol/champions/{champion}/build"
                     except:
                         await message.channel.send("Not a valid League Of Legends Champion.")
                     else:
-                        # set chrome options
-                        chrome_options = Options()
-                        chrome_options.headless = True
-                        
-                        # initialise driver
-                        driver = webdriver.Chrome(options=chrome_options, executable_path=r"C:\Users\seanh\Downloads\chromedriver_win32\chromedriver.exe")
+                        options = webdriver.ChromeOptions()
+                        options.add_argument("headless")
+                        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
                         driver.set_window_position(0, 0)
                         driver.set_window_size(1920, 1080)
                         driver.get(url)
-                        
+
                         # deal with privacy setting acceptance
-                        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME,'css-47sehv'))).click()
-                        
+                        WebDriverWait(driver, 10).until(
+                            ec.element_to_be_clickable((By.CLASS_NAME, "css-47sehv"))
+                        ).click()
+
                         # get rune div from u.gg
-                        element = driver.find_element(by=By.XPATH, value="/html/body/div[3]/div/div[2]/div[2]/div[2]/div/div/div/div[1]/div/div/div[5]/div/div[2]/div/div[1]")
+                        element = driver.find_element(
+                            by=By.XPATH,
+                            value="/html/body/div[3]/div/div[2]/div[2]/div/div/div/div/div/div/div/div[5]/div/div[2]/div/div[1]",
+                        )
                         file = BytesIO(element.screenshot_as_png)
-                        
+
                         # pm the user the div as an image
-                        await message.author.send(file=discord.File(file, 'image.png'))
+                        await message.author.send(file=discord.File(file, "image.png"))
                         driver.quit()
             else:
                 await message.channel.send("Please supply a champion.")
 
-        if cmd == 'build':
+        if cmd == "build":
             if len(message.content.split()) > 1:
                 champion = message.content.split()[1]
                 if type(champion) != str or len(message.content.split()) > 2:
                     await message.channel.send("Please only call the function via __*action* *champion* *game_mode*.")
                 else:
                     try:
-                        url = f'https://u.gg/lol/champions/{champion}/build'
+                        url = f"https://u.gg/lol/champions/{champion}/build"
                     except:
                         await message.channel.send("Not a valid League Of Legends Champion.")
                     else:
-                        # set chrome options
-                        chrome_options = Options()
-                        chrome_options.headless = True
-                        
-                        # initialise driver
-                        driver = webdriver.Chrome(options=chrome_options, executable_path=r"C:\Users\seanh\Downloads\chromedriver_win32\chromedriver.exe")
+                        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
                         driver.set_window_position(0, 0)
                         driver.set_window_size(1920, 1080)
                         driver.get(url)
-                        
+
                         # deal with privacy setting acceptance
-                        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME,'css-47sehv'))).click()
-                        
+                        WebDriverWait(driver, 10).until(
+                            ec.element_to_be_clickable((By.CLASS_NAME, "css-47sehv"))
+                        ).click()
+
                         # get build div from u.gg
-                        element = driver.find_element(by=By.XPATH, value="/html/body/div[3]/div/div[2]/div[2]/div/div/div/div/div/div/div/div[5]/div/div[7]")
+                        element = driver.find_element(
+                            by=By.XPATH,
+                            value="/html/body/div[3]/div/div[2]/div[2]/div/div/div/div/div/div/div/div[5]/div/div[9]",
+                        )
                         file = BytesIO(element.screenshot_as_png)
-                        
+
                         # pm the user the div as an image
-                        await message.author.send(file=discord.File(file, 'image.png'))
+                        await message.author.send(file=discord.File(file, "image.png"))
                         driver.quit()
             else:
                 await message.channel.send("Please supply a champion.")
 
-        if cmd == 'skills':
+        if cmd == "skills":
             if len(message.content.split()) > 1:
                 champion = message.content.split()[1]
                 if type(champion) != str or len(message.content.split()) > 2:
                     await message.channel.send("Please only call the function via __*action* *champion* *game_mode*.")
                 else:
                     try:
-                        url = f'https://u.gg/lol/champions/{champion}/build'
+                        url = f"https://u.gg/lol/champions/{champion}/build"
                     except:
                         await message.channel.send("Not a valid League Of Legends Champion.")
                     else:
-                        # set chrome options
-                        chrome_options = Options()
-                        chrome_options.headless = True
-                        
-                        # initialise driver
-                        driver = webdriver.Chrome(options=chrome_options, executable_path=r"C:\Users\seanh\Downloads\chromedriver_win32\chromedriver.exe")
+                        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
                         driver.set_window_position(0, 0)
                         driver.set_window_size(1920, 1080)
                         driver.get(url)
-                        
+
                         # deal with privacy setting acceptance
-                        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME,'css-47sehv'))).click()
-                        
+                        WebDriverWait(driver, 10).until(
+                            ec.element_to_be_clickable((By.CLASS_NAME, "css-47sehv"))
+                        ).click()
+
                         # get skills div from u.gg
-                        element = driver.find_element(by=By.XPATH, value="/html/body/div[3]/div/div[2]/div[2]/div/div/div/div/div/div/div/div[5]/div/div[4]")
+                        element = driver.find_element(
+                            by=By.XPATH,
+                            value="/html/body/div[3]/div/div[2]/div[2]/div/div/div/div/div/div/div/div[5]/div/div[6]",
+                        )
                         file = BytesIO(element.screenshot_as_png)
-                        
+
                         # pm the user the div as an image
-                        await message.author.send(file=discord.File(file, 'image.png'))
+                        await message.author.send(file=discord.File(file, "image.png"))
                         driver.quit()
             else:
                 await message.channel.send("Please supply a champion.")
@@ -131,34 +136,41 @@ async def on_message(message):
 @client.event
 async def on_presence_update(before, after):
     activity = after.activity
-    if activity and \
-        activity.name == 'League of Legends' and \
-        activity.state == 'In Game':
-        
+    if activity and activity.name == "League of Legends" and activity.state == "In Game":
         champion = activity.large_image_text.lower().replace("'", "").replace(".", "").replace(" ", "")
         user = client.get_user(after.id)
-        await user.send(f'Hey, I noticed you are playing {champion}. Here are some pieces of information that might help you out.')
-        url = f'https://u.gg/lol/champions/{champion}/build'
-        # set chrome options
-        chrome_options = Options()
-        chrome_options.headless = True
-        
-        # initialise driver
-        driver = webdriver.Chrome(options=chrome_options, executable_path=r"C:\Users\seanh\Downloads\chromedriver_win32\chromedriver.exe")
+        await user.send(
+            f"Hey, I noticed you are playing {champion}. Here are some pieces of information that might help you out."
+        )
+        url = f"https://u.gg/lol/champions/{champion}/build"
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
         driver.set_window_position(0, 0)
         driver.set_window_size(1920, 1080)
         driver.get(url)
-        
+
         # deal with privacy setting acceptance
-        WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.CLASS_NAME,'css-47sehv'))).click()
-        
-        element = driver.find_element(by=By.XPATH, value="/html/body/div[3]/div/div[2]/div[2]/div/div/div/div/div/div/div/div[5]/div/div[4]")
+        WebDriverWait(driver, 15).until(ec.element_to_be_clickable((By.CLASS_NAME, "css-47sehv"))).click()
+
+        element = driver.find_element(
+            by=By.XPATH,
+            value="/html/body/div[3]/div/div[2]/div[2]/div/div/div/div/div/div/div/div[5]/div/div[6]",
+        )
         file = BytesIO(element.screenshot_as_png)
-        await user.send(f"This is the skilling order for {champion}.", file=discord.File(file, 'image.png'))
-        
-        element = driver.find_element(by=By.XPATH, value="/html/body/div[3]/div/div[2]/div[2]/div/div/div/div/div/div/div/div[5]/div/div[7]")
+        await user.send(
+            f"This is the skilling order for {champion}.",
+            file=discord.File(file, "image.png"),
+        )
+
+        element = driver.find_element(
+            by=By.XPATH,
+            value="/html/body/div[3]/div/div[2]/div[2]/div/div/div/div/div/div/div/div[5]/div/div[9]",
+        )
         file = BytesIO(element.screenshot_as_png)
-        await user.send(f"These are the items you should be building for {champion}, in order.", file=discord.File(file, 'image.png'))
+        await user.send(
+            f"These are the items you should be building for {champion}, in order.",
+            file=discord.File(file, "image.png"),
+        )
         driver.quit()
 
-client.run(config('CLIENT_RUN_KEY'))
+
+client.run(config("CLIENT_RUN_KEY"))
---
+++ seed.py
@@ -0,0 +1,81 @@
+import cassiopeia as cass
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from io import BytesIO
+from pymongo import MongoClient
+from decouple import config
+
+client = MongoClient("mongodb://localhost:27017/")
+league_db = client["league_bot"]
+champions = league_db["champions"]
+
+cass.set_riot_api_key(config("RIOT_API_KEY"))
+
+
+def prep_champ_names():
+    full_champion_list = []
+    champ_list = cass.get_champions("EUW")
+    for league_champ in champ_list:
+        if league_champ.name.startswith("Nunu"):
+            full_champion_list.append("nunu")
+        elif league_champ.name.startswith("Renata"):
+            full_champion_list.append("renata")
+        else:
+            full_champion_list.append(league_champ.name.replace("'", "").replace(" ", "").lower())
+    return full_champion_list
+
+
+def seed_data():
+    champions.drop()
+    for name in prep_champ_names():
+        try:
+            url = f"https://u.gg/lol/champions/{name}/build"
+        except:
+            print("Could not find a champion profile at: ", f"https://u.gg/lol/champions/{name}/build")
+        else:
+            driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+            driver.set_window_position(0, 0)
+            driver.set_window_size(1920, 1080)
+            driver.get(url)
+
+            # deal with privacy setting acceptance
+            WebDriverWait(driver, 15).until(
+                ec.element_to_be_clickable((By.CLASS_NAME, "css-47sehv"))
+            ).click()
+
+            # get rune div from u.gg
+            element = driver.find_element(
+                by=By.XPATH,
+                value="/html/body/div[3]/div/div[2]/div[2]/div/div/div/div/div/div/div/div[5]/div/div[2]/div/div[1]",
+            )
+            rune_file = BytesIO(element.screenshot_as_png)
+
+            # get build div from u.gg
+            element = driver.find_element(
+                by=By.XPATH,
+                value="/html/body/div[3]/div/div[2]/div[2]/div/div/div/div/div/div/div/div[5]/div/div[9]",
+            )
+            build_file = BytesIO(element.screenshot_as_png)
+
+            # get skills div from u.gg
+            element = driver.find_element(
+                by=By.XPATH,
+                value="/html/body/div[3]/div/div[2]/div[2]/div/div/div/div/div/div/div/div[5]/div/div[6]",
+            )
+            skill_file = BytesIO(element.screenshot_as_png)
+
+            driver.quit()
+        champions.insert_one(
+            {"name": name,
+             "runes": rune_file.getvalue(),
+             "build": build_file.getvalue(),
+             "skills": skill_file.getvalue()
+             }
+        )
+
+
+seed_data()
